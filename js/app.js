7775e5daa3caa5b850395413a7b2e5fc2f164e8c
OdehYazan <eng.yazanodeh@gmail.com>
2021-04-26 17:16:50 +0300

    4 qusttions add

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
 'use stict';
 let userName = prompt('Tell me your name please ?').toUpperCase();
 let anime = prompt('Do you think I love Anime ? Y/N or Yes/No').toLowerCase();
-let videoGames = prompt('I love video games ? Y/N or Yes/No').toLowerCase();
-let karshat = prompt('I hate Karshat food ? Y/N or Yes/No').toLowerCase();
+
 while (anime !== 'yes' && anime !== 'y' && anime !== 'no' && anime !== 'n') {
   anime = prompt('Do you think I love Anime ? Y/N or Yes/No').toLowerCase();
 }
@@ -12,6 +11,8 @@ if (anime === 'yes' || anime === 'y') {
   console.log('I love Anime \'NO\' is a wrong answer');
 }
 
+let videoGames = prompt('I love video games ? Y/N or Yes/No').toLowerCase();
+
 while (videoGames !== 'yes' && videoGames !== 'y' && videoGames !== 'no' && videoGames !== 'n') {
   videoGames = prompt('I love video games ? Y/N or Yes/No').toLowerCase();
 }
@@ -20,12 +21,27 @@ if (videoGames === 'yes' || videoGames === 'y') {
 } else if (videoGames === 'no' || videoGames === 'n') {
   console.log('I love video games\'No\' is wrong answer');
 }
+
+let karshat = prompt('I hate Karshat food ? Y/N or Yes/No').toLowerCase();
+
 while (karshat !== 'yes' && karshat !== 'y' && karshat !== 'no' && karshat !== 'n') {
   karshat = prompt('I hate Karshat food ? Y/N or Yes/No').toLowerCase();
 }
-if (videoGames === 'yes' || videoGames === 'y') {
+if (karshat === 'yes' || karshat === 'y') {
   console.log('I hate Karshat food  \'yes\' is a wrong answer ');
-} else if (videoGames === 'no' || videoGames === 'n') {
-  console.log('ihate Karshat food \'No\' is  wrong answer');
+} else if (karshat === 'no' || karshat === 'n') {
+  console.log('I hate Karshat food \'No\' is  wrong answer');
 }
 
+let drivelicence = prompt ('Whta do think I have driving licence  ? Y/N or Yes/No');
+
+while (drivelicence !== 'yes' && drivelicence !== 'y' && drivelicence !== 'no' && drivelicence !== 'n') {
+  karshat = prompt('Whta do think I have driving licence or not  ? Y/N or Yes/No').toLowerCase();
+}
+if (drivelicence === 'yes' || drivelicence === 'y') {
+  console.log('Correct I have driving licence ');
+} else if (drivelicence === 'no' || drivelicence === 'n') {
+  console.log( 'Wrong  I have driving licence since 2010');
+}
+
+let favColor = prompt ()
